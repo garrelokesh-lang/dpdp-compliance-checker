cfde195691008ce62cb6a97174951fb439d377eb
Bharathreddy <bharathvirat28@gmail.com>
2026-04-24 14:09:00 +0530

    fixed syntax error in server.js

--- backend/server.js
+++ backend/server.js
@@ -407,5 +407,5 @@ app.get("/download/:id", async (req, res) => {
 =========================== */
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`):
+  console.log(`Server running on port ${PORT}`);
 });
